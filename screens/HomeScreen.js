e2576e5580e8ddfa8a3aa5c4be9384420acdadbf
Arjun Patrawala <ajpat1234@gmail.com>
2019-02-23 13:12:55 -0800

    home page almost done

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -27,17 +27,20 @@ let data = [
       {
         name: 'Hawaiian',
         price: 3.2,
-        rating: 4.2
+        rating: 4.2,
+        img: 'https://31p86334w2bvkz0249eyr0cr-wpengine.netdna-ssl.com/wp-content/uploads/2014/08/It-doesnt-get-much-better-than-Homemade-Hawaiian-Pizza.-Tropical-paradise-for-dinner-2.jpg'
       },
       {
         name: 'Cheese',
         price: 9.2,
-        rating: 4.1
+        rating: 4.1,
+        img: 'https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg'
       },
       {
         name: 'Pepperoni',
         price: 2.2,
-        rating: 4.9
+        rating: 4.9,
+        img: 'https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fcdn-image.myrecipes.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Fimage%2Frecipes%2Fck%2Fgluten-free-cookbook%2Fpepperoni-pizza-ck-x.jpg%3Fitok%3DNWreajsZ&w=450&c=sc&poi=face&q=85'
       }
     ]
   },
@@ -47,7 +50,8 @@ let data = [
       {
         name: 'Big Mac',
         price: 1.2,
-        rating: .1
+        rating: .1,
+        img: 'https://d12ph5aixxtnio.cloudfront.net/uploads/images/recipes/_full/veganbigmacstill.jpg'
       }
     ]
   }
@@ -61,9 +65,30 @@ export default class HomeScreen extends React.Component {
   renderCategory({ item, index }) {
 
     function renderFood({ item }) {
+
       return (
         <View>
-          <Text>{item.name}</Text>
+          <Image
+            source={{
+              uri: item.img
+            }}
+            style={{
+              width: 100,
+              height: 100,
+              borderRadius: 8,
+              marginRight: 8
+            }}
+          ></Image>
+          <Text
+            style={{
+              fontSize: 16
+            }}
+          >{item.name}</Text>
+          <Text
+            style={{
+              fontSize: 14
+            }}
+          >${item.price}</Text>
         </View>
       );
     }
@@ -92,10 +117,13 @@ export default class HomeScreen extends React.Component {
         }}
       >
         <Text style={{
-          fontSize: 20,
+          fontSize: 24,
           fontWeight: 'bold'
         }}>{item.category}</Text>
         <FlatList
+          style={{
+            marginTop: 4
+          }}
           horizontal={true}
           contentContainerStyle={styles.horizontalList}
           data={item.foods}
