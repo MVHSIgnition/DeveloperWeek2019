e8a7d6f2355d7578a5e477854c66a29105445d2e
Jonathan Liu <liu.z.jonathan@gmail.com>
2019-02-23 13:13:24 -0800

    basic 2nd screen

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,17 +1,52 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
 
-import { View } from 'react-native';
+import { 
+  View,
+  FlatList,
+  Text,
+} from 'react-native';
+
+let data = [
+  {
+    key: '0',
+    name: 'Hawaiian',
+    price: 3.2,
+    rating: 4.2
+  },
+  {
+    key: '1',
+    name: 'Pepperoni',
+    price: 6.9,
+    rating: 3.4
+  }
+];
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
-    title: 'Links',
+    title: 'INSERT CATEGORY HERE',
   };
+
+  renderItem({ item, index }) {
+    return (
+      <View>
+        <View style={{ marginLeft: '25%' }}>
+          <Text style={{ fontWeight: 'bold' }}>{item.name}</Text>
+          <Text>Price: {item.price}</Text>
+          <Text>Rating: {item.rating}</Text>
+        </View>
+      </View>
+    );
+  }
 
   render() {
     return (
       <View>
-        Wassup!
+        <FlatList
+          contentContainerStyle={styles.mainList}
+          data={data}
+          renderItem={this.renderItem}
+        />
       </View>
     );
   }
@@ -23,4 +58,7 @@ const styles = StyleSheet.create({
     paddingTop: 15,
     backgroundColor: '#fff',
   },
+  mainList: {
+    padding: 10
+  }
 });
